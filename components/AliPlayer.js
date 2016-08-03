ad4322c7eb75f2afddc822eb0cda72220fd45565
shaojia <jia.shao@eastiming.com>
2016-08-03 17:49:49 +0800

    add seek

--- components/AliPlayer.js
+++ components/AliPlayer.js
@@ -8,17 +8,24 @@ export default class AliPlayer extends Component {
     super(props);
   }
 
+  player;
+
   componentDidMount() {
     this.initPlayerApi();
   }
 
+  _handleSeek = () => {
+    this.player.seek(10);
+  };
+
   initPlayerApi = () => {
     //const url = 'rtmp://live.hkstv.hk.lxdns.com/live/hks';
-    const url = 'http://58.55.127.28/m3u8/cctv5_1800/desc.m3u8';
+    //const url = 'http://58.55.127.28/m3u8/cctv5_1800/desc.m3u8';
     //const url = 'http://58.55.127.28/m3u8/cctv5jiaHD_1800/desc.m3u8';
     //const url = 'http://58.55.127.28/m3u8/cctv1HD_1800/desc.m3u8';
+    const url = 'http://video.easub.com/6c413a3e-f7c7-4810-b88e-2273381cef52/1470205800458.m3u8';
 
-    const player = new prismplayer({
+    this.player = new prismplayer({
       id: "J_prismPlayer", // 容器id
       source: url,         // 视频url 支持互联网可直接访问的视频地址
       autoplay: true,      // 自动播放
@@ -33,6 +40,7 @@ export default class AliPlayer extends Component {
         <div style={{ width: '800px', height: '450px', margin: '0 auto' }}>
           <div id='J_prismPlayer' className='prism-player'></div>
         </div>
+        <button onClick={this._handleSeek}>Seek</button>
       </div>
     )
   }
--- components/JwPlayer.js
+++ components/JwPlayer.js
@@ -12,10 +12,17 @@ export default class JwPlayer extends Component {
     this.setupVideo();
   }
 
+  _handleSeek = () => {
+    jwplayer(this.refs.video).seek(10);
+  };
+
   setupVideo(){
+    const url = 'http://video.easub.com/6c413a3e-f7c7-4810-b88e-2273381cef52/1470205800458.m3u8';
+    //const url = 'rtmp://live.hkstv.hk.lxdns.com/live/hks';
+
     jwplayer(this.refs.video).setup({
       playlist: [{
-        sources: [{"file":'rtmp://live.hkstv.hk.lxdns.com/live/hks',"type":'rtmp'}]
+        sources: [{"file":url,"type":'m3u8'}]
       }],
       width: '400',
       height: '225',
@@ -31,6 +38,7 @@ export default class JwPlayer extends Component {
     return (
       <div>
         <div ref="video" id="video">Video Jw</div>
+        <button onClick={this._handleSeek}>Seek</button>
       </div>
     )
   }
